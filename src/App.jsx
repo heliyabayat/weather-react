34e07afaa9d4652ac889ab447547cea6254d9b37
Heliya Bayat Makoo <121420178+heliyabayat@users.noreply.github.com>
2023-09-04 11:00:18 +0330

    add github source link

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ function App() {
   return (
     <>
       <SearchBar />
+      <p>
+        <a href="https://github.com/heliyabayat/weather-react">
+          open-source code
+        </a>
+        by<a href="https://heliyabayat.github.io/">Heliya Bayat Makoo</a>
+      </p>
     </>
   );
 }
